aa267611231e7a049c056da4a15aef41ce94671f
MohamedEmad2002 <MohamedElbakrawy70@gmail.com>
2023-02-28 20:59:38 +0200

    add screen "ServeTwo"

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:sw_project/ui/screens/login_signup/worker_sign_up.dart';
 import 'package:sw_project/ui/screens/requests/send_request.dart';
 import 'package:sw_project/ui/screens/requests/veiw_request.dart';
 import 'package:sw_project/ui/screens/serve/ServeOne.dart';
+import 'package:sw_project/ui/screens/serve/ServeTwo.dart';
 
 void main() {
   runApp(
@@ -21,7 +22,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      home: ServeOne(),
+      debugShowCheckedModeBanner: false,
+      home: ServeTwo(),
     );
   }
 }
