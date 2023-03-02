fbe238594625206efb7e1b79a112904cd43c8e54
MohamedEmad2002 <MohamedElbakrawy70@gmail.com>
2023-03-02 13:35:41 +0200

    Add home page

--- lib/ui/screens/Home/HomePage.dart
+++ lib/ui/screens/Home/HomePage.dart
@@ -9,58 +9,65 @@ class HomePage extends StatelessWidget {
         body: SafeArea(
             child: Column(
       children: [
-        Container(
-          decoration: const BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(40.0),
-                  bottomLeft: Radius.circular(40.0))),
-          child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(10),
-                color: Colors.blue,
-                child: Row(
-                  children: const [
-                    Icon(
-                      Icons.notifications_none,
-                      color: Colors.white,
-                    )
-                  ],
-                ),
+        Column(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(40.0),
+                      bottomLeft: Radius.circular(40.0))),
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.blue,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: const [
+                        Icon(
+                          Icons.notifications_none,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 170,
+                    child: Column(
+                      children: const [
+                        Text(
+                          "خدماتك",
+                          style: TextStyle(fontSize: 40, color: Colors.white),
+                        )
+                      ],
+                    ),
+                  )
+                ],
               ),
-              SizedBox(
-                height: 140,
-                child: Column(
-                  children: const [
-                    Text(
-                      "خدماتك",
-                      style: TextStyle(fontSize: 40, color: Colors.white),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+            ),
+          ],
         ),
-        Padding(
+        Container(
+          //margin: const EdgeInsets.all(10),
           padding: const EdgeInsets.all(20.0),
           child: Material(
+            elevation: 5.0,
             child: TextFormField(
               textAlign: TextAlign.end,
               keyboardType: TextInputType.name,
               decoration: InputDecoration(
-                  prefixIcon: IconButton(
-                      onPressed: () {}, icon: const Icon(Icons.search)),
-                  hintText: "ابحث عن الخدمه التي تحتاجها",
-                  hintStyle: const TextStyle(color: Colors.blue, fontSize: 20),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide: const BorderSide(color: Colors.blue, width: 2)),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide:
-                          const BorderSide(color: Colors.white38, width: 2))),
+                prefixIcon: IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.search)),
+                hintText: "ابحث عن الخدمه التي تحتاجها",
+                hintStyle: const TextStyle(color: Colors.blue, fontSize: 20),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    borderSide: const BorderSide(color: Colors.white30, width: 2)),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    borderSide: const BorderSide(color: Colors.white30, width: 2)),
+              ),
             ),
           ),
         ),
